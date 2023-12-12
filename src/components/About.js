559198c79d91697c8cbeefc8d642b237b74ac16f
BenChris090 <benedictchrstn@gmail.com>
2023-12-12 12:19:24 +0100

    Steady progress on the pages

--- src/components/About.js
+++ src/components/About.js
@@ -4,58 +4,62 @@ import { NavLink,Link } from "react-router-dom";
 import { FaAt, FaStar, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const About = () => {
-    let ACCOUNTS = require("../images/bg1.jpg");
-    let TRAVEL = require("../images/bg1.jpg");
-    let TYPEWRITER = require("../images/bg1.jpg");
-    let LOANS = require("../images/bg1.jpg");
+    let INDEXP = require("../images/INDEXP.jpg");
+    let QM = require("../images/GM.jpg");
+    let TYPEWRITER = require("../images/CE.jpg");
+    let CCA = require("../images/CCA.jpg");
 
     return ( 
         <div className="about h-full">
             <Navbar/>
             
-            <div className="header text-white px-6 lg:px-24 py-10 bg-[url('./images/bg1.jpg')] bg-opacity-50 bg-no-repeat bg-center bg-cover space-y-2">
-                <h1 className="text-3xl lg:text-4xl"><b>About VOLUNTEER ENERGY</b></h1>
-                <p className="text-xl lg:max-w-xl">Volunteer Energy was Established in 2000 with a license of an European broker regulated by the 
-                 Conduct Authority (FCA), license No. 191/12.</p>
-                <ul className="flex text-white lg:pt-5">
-                    <FaAt className="text-3xl lg:text-4xl hover:text-cyan-700 ease-in duration-500"/>
-                    <Link to="https://twitter.com"><FaTwitter className="text-3xl lg:text-4xl ml-4 lg:ml-20 hover:text-cyan-700 ease-in duration-500"/></Link>
-                    <Link to="https://instagram.com"><FaInstagram className="text-3xl lg:text-4xl ml-4 lg:ml-20 hover:text-cyan-700 ease-in duration-500"/></Link>
-                    <Link to="https://linkedin.com"><FaLinkedin className="text-3xl lg:text-4xl ml-4 lg:ml-20 hover:text-cyan-700 ease-in duration-500"/></Link>
+            <div className="header text-white px-6 lg:px-24 py-10 bg-[url('./images/bg4.jpg')] bg-no-repeat bg-cover bg-center space-y-2">
+                <h1 className="text-2xl lg:text-4xl"><b>About Volunteer Energy</b></h1>
+                <p className="text-xl text-justify lg:max-w-xl">At Volunteer Energy, we illuminate paths to success in the energy and general merchandise sectors. 
+                Established on the pillars of innovation, sustainability, and client-centricity, Volunteer Energy is your comprehensive partner in oil and gas consulting 
+                and high-quality general merchandise services.</p>
+                <ul className="flex text-white items-center lg:pt-5 lg:px-2">
+                    <FaAt className="text-3xl lg:text-4xl hover:text-pink-600 ease-in duration-500"/>
+                    <Link to="https://twitter.com"><FaTwitter className="text-3xl lg:text-4xl ml-4 lg:ml-20 hover:text-pink-600 ease-in duration-500"/></Link>
+                    <Link to="https://instagram.com"><FaInstagram className="text-3xl lg:text-4xl ml-4 lg:ml-20 hover:text-pink-600 ease-in duration-500"/></Link>
+                    <Link to="https://linkedin.com"><FaLinkedin className="text-3xl lg:text-4xl ml-4 lg:ml-20 hover:text-pink-600 ease-in duration-500"/></Link>
                 </ul>
             </div>
             <div className="body text-left space-y-2 px-6 lg:px-24 py-8">
-                <h1 className="text-2xl lg:text-3xl p-2"><b>Get loan with minimal documents</b></h1>
-                <p className="text-lg text-justify lg:text-xl lg:text-left text-gray-700 font-medium py-2 px-2">Octillion Whales Bank leads the pack when it comes to Banking. Whether you’re a skilled or completely new to online banking, 
-                our platforms give you the tools needed to take advantage of the foreign exchange market’s volatility. With Heinrich Trust Bank it’s simple to get started trading Forex, and it’s easy to get the education needed to hone your skills. 
-                You’ll be able to benefit from one on one support from our specialist team and develop your expertise with our professional Forex trading resources and insights. For beginner and advanced traders alike we offer high leverage, guaranteed stop losses on every Forex trade, 
-                and our fixed spreads remain constant to give you greater strategic power and control. Get Started with us Today, and experience a swift journey filled with excitement.</p>
+                <h1 className="text-2xl lg:text-3xl p-2"><b>Our Story</b></h1>
+                <p className="text-lg lg:text-xl lg:text-justify text-gray-700 font-medium py-2 px-2">Founded with a vision to redefine excellence, 
+                Volunteer Energy is the product of industry expertise and a passion for making a positive impact. Our journey began with a commitment to providing innovative solutions 
+                and a diverse range of top-quality general merchandise.</p>                
+                <h1 className="text-2xl lg:text-3xl p-2"><b>What Sets Us Apart</b></h1>
                 <ul className="space-y-2">
-                    <li className="flex text-lg text-gray-700 font-medium items-centFa"><FaStar className="text-cyan-900 mr-2"></FaStar> Internet Banking</li>
-                    <li className="flex text-lg text-gray-700 font-medium items-center"><FaStar className="text-cyan-900 mr-2"></FaStar> Loans</li>
-                    <li className="flex text-lg text-gray-700 font-medium items-center"><FaStar className="text-cyan-900 mr-2"></FaStar> Funds Transfer</li>
-                    <li className="flex text-lg text-gray-700 font-medium items-center"><FaStar className="text-cyan-900 mr-2"></FaStar> Prepaid Cards</li>
-                    <li className="flex text-lg text-gray-700 font-medium items-center"><FaStar className="text-cyan-900 mr-2"></FaStar> MCash</li>
+                    <li className="flex text-lg px-2 text-gray-700 font-medium items-center"><FaStar className="text-pink-600 mr-2"></FaStar> Industry Expertise</li>
+                    <li className="flex text-lg px-2 text-gray-700 font-medium items-center"><FaStar className="text-pink-600 mr-2"></FaStar> Quality Merchandise</li>
+                    <li className="flex text-lg px-2 text-gray-700 font-medium items-center"><FaStar className="text-pink-600 mr-2"></FaStar> Reliable customer service</li>
+                    <li className="flex text-lg px-2 text-gray-700 font-medium items-center"><FaStar className="text-pink-600 mr-2"></FaStar> Client-Centric Approach</li>
                 </ul>
+                <h1 className="text-2xl lg:text-3xl p-2"><b>Our Commitment</b></h1>
+                <p className="text-lg text-justify lg:text-xl lg:text-justify text-gray-700 font-medium py-2 px-2">Volunteer Energy is more than a business; 
+                it's a commitment to excellence, sustainability, and positive impact across the energy and general merchandise sectors. We are dedicated to creating a brighter 
+                and more sustainable future for our clients, partners, and communities..</p>   
             </div>
             <div className="body text-center space-y-2 px-6 lg:px-48 py-8">
-                <h1 className="text-2xl lg:text-3xl p-2"><b>World Leading Finance Institution</b></h1>
-                <p className="text-lg lg:text-xl text-gray-700 font-medium py-2 px-2">The Financial Conduct Authority (FCA) license No. 191/13 makes the company follow strict quality standards and transparency of its activities, 
-                including the provision of client services.</p>
+                <h1 className="text-2xl lg:text-3xl p-2"><b>World Leading Energy Institution</b></h1>
+                <p className="text-lg lg:text-xl text-gray-700 font-medium py-2 px-2">Our dedication to excellence is demonstrated through rigorous adherence to industry standards and transparent practices. 
+                Whether in oil and gas consulting or general merchandise services, our commitment to quality extends to every facet of our operations, ensuring exceptional client experiences.</p>
             </div>
             <div className="body text-left px-6 lg:px-24 py-8 space-y-4 lg:space-x-4 lg:space-y-0 lg:flex">
                 <div className="w-full space-y-2 shadow-xl rounded-xl border-2 border-black lg:w-1/4 py-10 px-4 hover:scale-105">
-                    <img src={ACCOUNTS} alt="debit_card" className="w-1/4"/>
-                    <h2 className="text-xl font-semibold">Accounts</h2>
-                    <p>We offer a wide range of accounts to suit lifestyles & needs. 
-                        Free and paid-for add-ons are available to give total control. 
-                        Download, Fill & Submit at our Branch.</p>
+                    <img src={INDEXP} alt="debit_card" className="w-1/4"/>
+                    <h2 className="text-xl font-semibold">Industry Expertise</h2>
+                    <p>Our seasoned professionals bring extensive experience to the table, 
+                        ensuring strategic insights and effective solutions in both oil 
+                        and gas consulting and the general merchandise landscape..</p>
                 </div>
                 <div className="w-full space-y-2 shadow-xl rounded-xl border-2 border-black lg:w-1/4 py-10 px-4 hover:scale-105">
-                    <img src={TRAVEL} alt="credit_card" className="w-1/4"/>
-                    <h2 className="text-xl font-semibold">Round The Clock Banking</h2>
-                    <p>Access your personal account information with ease, transfer funds securely whenever you want, 
-                        wherever you want..</p>
+                    <img src={QM} alt="credit_card" className="w-1/4"/>
+                    <h2 className="text-xl font-semibold">Quality Merchandise</h2>
+                    <p>At Volunteer Energy, we pride ourselves on curating a diverse range of high-quality general merchandise. 
+                        Our commitment to excellence extends to every product we offer</p>
                 </div>
                 <div className="w-full space-y-2 shadow-xl rounded-xl border-2 border-black lg:w-1/4 py-10 px-4 hover:scale-105">
                     <img src={TYPEWRITER} alt="prepaid_card" className="w-1/4"/>
@@ -64,9 +68,10 @@ const About = () => {
                         superior financial performance and creating role models for society.</p>
                 </div>
                 <div className="w-full space-y-2 shadow-xl rounded-xl border-2 border-black lg:w-1/4 py-10 px-4 hover:scale-105">
-                    <img src={LOANS} alt="mobile_wallet" className="w-1/4"/>
-                    <h2 className="text-xl font-semibold">Loans</h2>
-                    <p>We offer different kinds of loans Students Loan, Business banking and more.</p>
+                    <img src={CCA} alt="mobile_wallet" className="w-1/4"/>
+                    <h2 className="text-xl font-semibold">Client-Centric Approach</h2>
+                    <p>We don't just do business; we build relationships. Our client-centric approach 
+                        involves understanding the unique needs of our clients and delivering tailored solutions that contribute to their success.</p>
                 </div>
             </div>
             <FootBar/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,18 +21,20 @@ const Navbar = () => {
     }
     
     return ( 
-        <nav className="flex p-2 px-6 items-center justify-between bg-white">
+        <nav className="flex p-2 px-6 items-center justify-between space-x-auto max-w-full bg-white">
             {/* mobile nav starts here */}
-            <div id="mNav" className="h-screen w-96 hidden z-0 fixed left-0 top-0 bg-white overflow-x-hidden overflow-y-auto pt-28 ease-in duration-500">
-                <FaTimes className="text-2xl text-sky-600 absolute top-5 right-8" onClick={setNav}/>
+            <div id="mNav" className="h-screen w-screen space-y-auto text-center items-center justify-center hidden z-0 fixed left-0 top-0 bg-white overflow-x-hidden overflow-y-auto ease-in duration-500">
+                <div className="p-4 m-2 h-3/4">
+                <FaTimes className="text-2xl text-sky-600 ml-auto mb-8" onClick={setNav}/>
                 <NavLink to="/" style={({ isActive }) => (isActive ? {color: '#0f93d7'} : {color: '#2F4F4F'})}  className="mt-5 text-center space-y-2 block text-xl">Home</NavLink>
                 <NavLink to="/corporate-banking" style={({ isActive }) => (isActive ? {color: '#0f93d7'} : {color: '#2F4F4F',})}  className="mt-10 text-center space-y-2 block text-xl">Corporate</NavLink>
                 <NavLink to="/about" style={({ isActive }) => (isActive ? {color: '#0f93d7'} : {color: '#2F4F4F'})}  className="mt-10 text-center space-y-2 block text-xl">About Us</NavLink>
                 <NavLink to="/contact" style={({ isActive }) => (isActive ? {color: '#0f93d7'} : {color: '#2F4F4F'})}  className="mt-10 text-center space-y-2 block text-xl">Contact Us</NavLink>
+                </div>
             </div>
             {/* mobile nav ends here */}
-            <Link to="/" className="flex items-center"><img src={ VOLUNTEER_ENERGY_LOGO } className="h-12 mr-3 sm:h-16 lg:h-16" alt="Logo" /><span className="self-center text-3xl font-semibold whitespace-nowrap text-sky-600 lg:text-4xl">VOLUNTEER <span className="text-pink-600">ENERGY</span></span></Link>
-            <div className="bars space-x-2 flex items-center ml-36">
+            <Link to="/" className="flex items-center"><img src={ VOLUNTEER_ENERGY_LOGO } className="h-12 mr-1 lg:mr-3 sm:h-16 lg:h-16" alt="Logo"/><span className="self-center text-lg font-semibold whitespace-nowrap text-sky-600 lg:text-4xl">VOLUNTEER <span className="text-pink-600">ENERGY</span></span></Link>
+            <div className="bars space-x-2 flex items-center ml-auto">
                 <FaBars className="text-2xl flex text-sky-600 my-2 lg:hidden" onClick={setNav}/>
             </div>
             <div className="links hidden items-center text-center lg:flex lg:py-auto lg:space-x-14">
