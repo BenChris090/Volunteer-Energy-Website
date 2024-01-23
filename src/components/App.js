f1ad0bba38e6d9f8c70e68684fd2ec9c7187125e
BenChris090 <benedictchrstn@gmail.com>
2024-01-23 09:42:57 +0100

    APP DISABLE UPDATE

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,15 @@ import Contact from './Contact';
 import Four0Four from './404';
 
 function App() {
+  // Check if the DISABLE_APP environment variable is set
+  const isAppDisabled = process.env.REACT_APP_DISABLE_APP;
   return (
     <Router>
       <div className="App h-full">
         <div className="content">
+          {isAppDisabled ? (
+            <p>App is temporarily disabled.</p>
+          ) : (
           <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/about' element={<About/>}/>
@@ -16,6 +21,7 @@ function App() {
             <Route path='/404' element={<Four0Four/>}/>
           	<Route path="*" element={<Navigate to="/404" />}/>
           </Routes>
+          )}
         </div>        
       </div>
     </Router>
